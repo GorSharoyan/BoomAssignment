f30be52fbc53764631dd9416c3279dc40cd86556
Zen 0ne <gor.sharoyan95@gmail.com>
2021-11-30 01:19:01 +0400

    :construction_worker:logic(services/printPattern.services.js)added logic of pattern printin in a separate folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+
+//services imports
+import { printNumberPatternUp } from "./services/printPattern.services";
+import { printPatternDown } from "./services/printPattern.services";
 
 function App() {
   let [value, setValue] = useState("");
 
-  function createImage(number) {
-    let result = "";
-    for (let i = 0; i < number; i++) {
-      for (let j = 0; j < number; j++) {
-        result += "*";
-      }
-    }
-  }
-
   const handleInputChange = (event) => {
-    setValue(event.target.value);
+    let number = event.target.value;
   };
 
   return (
---
+++ src/services/printPattern.services.js
@@ -0,0 +1,22 @@
+export function printNumberPatternUp(n) {
+  let res = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 1; j <= n - i; j++) {
+      res += "*";
+      a.append(<p></p>);
+    }
+    res += "\n";
+  }
+  console.log(result);
+}
+
+export function printPatternDown(number) {
+  let result = "";
+  for (let i = 1; i < number; i++) {
+    for (let j = 1; j <= i; j++) {
+      res += "*";
+    }
+    res += "\n";
+  }
+  console.log(result);
+}
